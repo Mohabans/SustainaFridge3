cdd9069471b7432ff7dd39bf116f7d6049d50034
Thonks123 <sande501@msu.edu>
2024-01-27 21:58:53 -0500

    Added Button and edit view

--- app/src/main/java/com/example/sustainafridge3/MainActivity.kt
+++ app/src/main/java/com/example/sustainafridge3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.sustainafridge3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mButton: Button = findViewById(R.id.button)
+
+        mButton.setOnClickListener {
+            // Code to be executed when the button is clicked
+            // ...
+            // ...
+            // ...
+        }
     }
 }
