78fd817a5b57601a72a40942c57afcb0dd62ab46
Thonks123 <sande501@msu.edu>
2024-01-27 22:07:32 -0500

    Added UI Elements to MainActivity and added listeners

--- app/src/main/java/com/example/sustainafridge3/MainActivity.kt
+++ app/src/main/java/com/example/sustainafridge3/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.sustainafridge3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var item1 = Item("Milk")
+        var item2 = Item("Grapes")
+        var item3 = Item("Yogurt")
+        val items = mutableListOf(Item("Apple"), Item("Milk"), Item("Eggs"))
+        var testFridge = Fridge(items)
+
         val mButton: Button = findViewById(R.id.button)
+        val mEditText: EditText = findViewById(R.id.editText1)
+        val mTextView: TextView = findViewById(R.id.textView)
 
         mButton.setOnClickListener {
-            // Code to be executed when the button is clicked
-            // ...
-            // ...
-            // ...
+
+            val userInput: String = mEditText.toString()
+            mTextView.setText(userInput)
+
         }
     }
 }
